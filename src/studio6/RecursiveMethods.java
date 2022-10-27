eeb9e0f9bd2f0b43822809b8985f68386340cd37
jordanoliver <jordanoliver@localhost>
2022-10-27 14:20:13 -0500

    studio 6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,9 +12,13 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+		if (n==0) {
 			return 0;
+		}
+		else {
+			return geometricSum(n-1)+1/Math.pow(2,n);
+		}
+			// FIXME compute the geometric sum for the first n terms recursively
 		
 	}
 
@@ -27,12 +31,42 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
+		int a = q;
+		
+		if (p%q==0) {
+			return q;
+		}
+		else {
+			q = p%q;
+			p = a;
+			return gcd(p, q);
+		}
+//			for (int i=q;i>0;i++)
+//			{
+//				if (q%i==0 && (p%q)%i==0) {
+//					result = i;
+//				}
+//			}
+//			return gcd(q);
+		}
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+//			return 0;
 		
-	}
-
+	
+	public static int[] toReversedHelper(int[] array, int index) {
+		index = array.length / 2;
+		int lastIndex = array.length - 1;
+		int mirrorIndex = lastIndex - index;
+		int[] result = new int[array.length];
+		result[index] = array[mirrorIndex];
+		result[mirrorIndex] = array[index];
+		result = toReversedHelper(array, index);
+		
+		// FIXME create a helper method that can recursively reverse the given array
+		return result;
+	
+}
 	
 
 	/**
@@ -42,10 +76,17 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
+		int[] result = new int[array.length];
+		if (array.length == 0) {
+			return result;
+		}
+		else if (array.length == 1) {
+			return result;
+		}
+		else {
+			return toReversedHelper(array, array.length/2);
+		}
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
 	}
 
 	/**
